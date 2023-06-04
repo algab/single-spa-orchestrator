03957271b79fd9620b6b0bc5d014f00ab0d18fef
Álvaro Oliveira <algab.oliveira@gmail.com>
2023-06-04 12:01:19 -0300

    fix in layout assembly flow

--- orchestration.js
+++ orchestration.js
@@ -6,13 +6,18 @@ System.register(['single-spa'], () => {
       singleSpa.registerApplication({
         name: '@test/mfe-page-one',
         app: () => System.import('@test/mfe-page-one'),
-        activeWhen: (location) => location.pathname === '/',
+        activeWhen: (location) => location.pathname === '/' && (location.hash === '' || location.hash === '#/'),
       });
       singleSpa.registerApplication({
         name: '@test/mfe-page-two',
         app: () => System.import('@test/mfe-page-two'),
-        activeWhen: (location) => location.pathname === '/two',
+        activeWhen: (location) => location.pathname === '/two' || location.hash === '#/two' || location.hash === '#/two/',
       });
+      singleSpa.registerApplication({
+        name: '@test/mfe-header',
+        app: () => System.import('@test/mfe-header'),
+        activeWhen: () => true,
+      })
       singleSpa.start();
     }
   }
